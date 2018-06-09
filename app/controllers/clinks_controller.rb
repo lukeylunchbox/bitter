9b7c0cc674d3789dc2da40e45ba239c42d5439c7
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-06-09 14:32:41 +1000

    Add final touches

--- app/controllers/clinks_controller.rb
+++ app/controllers/clinks_controller.rb
@@ -2,29 +2,21 @@ class ClinksController < ApplicationController
   before_action :set_clink, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /clinks
-  # GET /clinks.json
   def index
     @clinks = Clink.all.order("created_at DESC")
     @clink = Clink.new
   end
 
-  # GET /clinks/1
-  # GET /clinks/1.json
   def show
   end
 
-  # GET /clinks/new
   def new
     @clink = current_user.clinks.build
   end
 
-  # GET /clinks/1/edit
   def edit
   end
 
-  # POST /clinks
-  # POST /clinks.json
   def create
     @clink = current_user.clinks.build(clink_params)
 
@@ -39,8 +31,6 @@ class ClinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clinks/1
-  # PATCH/PUT /clinks/1.json
   def update
     respond_to do |format|
       if @clink.update(clink_params)
@@ -53,8 +43,6 @@ class ClinksController < ApplicationController
     end
   end
 
-  # DELETE /clinks/1
-  # DELETE /clinks/1.json
   def destroy
     @clink.destroy
     respond_to do |format|
@@ -64,12 +52,11 @@ class ClinksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
     def set_clink
       @clink = Clink.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def clink_params
       params.require(:clink).permit(:clink)
     end
